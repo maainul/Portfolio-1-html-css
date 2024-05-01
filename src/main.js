e48149d08f2627bfb691c306b18dc5c448be7061
maainul <mainul080@yahoo.com>
2024-05-01 19:02:13 +0600

    About Section

--- src/main.js
+++ src/main.js
@@ -1,4 +1,6 @@
 import '../styles/style.css';
 import '../styles/components/header.css';
 import '../styles/components/hero.css';
+import '../styles/components/about.css';
+
 import '../styles/utils.css';
