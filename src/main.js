e9f523c4375a51ea5fc591f08b8c713c03a3c2e8
maainul <mainul080@yahoo.com>
2024-05-01 10:05:43 +0600

    Hero and nav

--- src/main.js
+++ src/main.js
@@ -1,2 +1,4 @@
-import '../styles/style.css'
-import '../styles/utils.css'
+import '../styles/style.css';
+import '../styles/components/header.css';
+import '../styles/components/hero.css';
+import '../styles/utils.css';
